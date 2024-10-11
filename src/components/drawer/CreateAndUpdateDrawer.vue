92f580307954cda2a8deb3d73b9400cfc64790e6
JamshidDev <raximovj662@gmail.com>
2024-10-11 19:26:13 +0500

    added

---
+++ src/components/drawer/CreateAndUpdateDrawer.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+const props = defineProps({
+  title:{
+    type:string,
+    default:null,
+  },
+
+})
+</script>
+
+<template>
+  <n-drawer
+      v-model:show="active"
+      :default-width="502"
+      placement="right"
+      resizable
+  >
+    <n-drawer-content>
+      <template #header>
+        {{title}}
+      </template>
+    </n-drawer-content>
+  </n-drawer>
+</template>
+
+<style scoped>
+
+</style>
