95e8e7b5122e00cca4d8b90e6e8fae96f14ba327
JamshidDev <raximovj662@gmail.com>
2025-02-12 09:11:21 +0500

    fixed lms route bug

--- src/router/modules/lmsRoute.js
+++ src/router/modules/lmsRoute.js
@@ -1,17 +1,10 @@
 
 import {AppPaths} from "@/utils/index.js"
 import Utils from "@/utils/Utils.js"
-import ExamPage from "@/pages/lms/exam/ExamPage.vue"
 import TopicPage from "@/pages/lms/topic/TopicPage.vue"
-import TopicDetailPage from "@/pages/lms/topicDetail/topicDetailPage.vue"
+import topicDetailPage from "@/pages/lms/topicDetail/topicDetailPage.vue"
 
 export default [
-    {
-        path:Utils.routeLmsPathMaker(AppPaths.Exam),
-        name:AppPaths.Exam.substring(1),
-        component:ExamPage,
-        children:[],
-    },
     {
         path:Utils.routeLmsPathMaker(AppPaths.Topic),
         name:AppPaths.Topic.substring(1),
@@ -20,7 +13,7 @@ export default [
     },
     {
         path:Utils.routeLmsPathMaker(AppPaths.Topic+'/:id'),
-        component:TopicDetailPage,
+        component:topicDetailPage,
         children:[],
     },
 ]
