d028574609eca1cbd0f5beafaa320c275b9b297c
shakhrillodev <shaxrillonusratov@gmail.com>
2025-04-27 14:49:31 +0500

    fix bug

--- src/layouts/combo/ui/HelperLayout.vue
+++ src/layouts/combo/ui/HelperLayout.vue
@@ -1,14 +1,9 @@
 <script setup>
 import OrganizationModal from "@/pages/app/profile/ui/OrganizationModal.vue"
 import TelegramPopup from "@/pages/app/popups/Telegram.vue"
-onMounted(()=>{
-  console.log("Helpers mounted")
-})
 </script>
 
 <template>
-  <div>
     <OrganizationModal/>
     <TelegramPopup/>
-  </div>
 </template>
