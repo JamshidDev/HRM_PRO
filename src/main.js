9510f61e41f1ae2a633f7e347bae43406bcdb5d2
JamshidDev <raximovj662@gmail.com>
2025-05-07 15:37:19 +0500

    upgrade tailwindcss

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from "./router"
 import {createPinia} from "pinia"
 import i18n from "./i18n/index.js"
+import pinia from "./store/index.js"
 import ApiService from "@/service/ApiService.js";
 import naive from 'naive-ui'
 import mask from "./directives/mask.js"
@@ -17,11 +18,6 @@ const app = createApp(App);
 const meta = document.createElement('meta')
 meta.name = 'naive-ui-style'
 document.head.appendChild(meta)
-
-
-
-
-const pinia =  createPinia()
 
 app.use(VueSignature)
 app.use(naive)
