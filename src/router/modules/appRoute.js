f8b24a6f9432f6e0ded7679f74923c2ee4681db5
JamshidDev <raximovj662@gmail.com>
2025-10-10 11:22:49 +0500

    last production version

--- src/router/modules/appRoute.js
+++ src/router/modules/appRoute.js
@@ -1,10 +1,8 @@
 import {AppLayouts, AppPaths} from "@/utils/index.js"
 import HomePage from "@/pages/app/home/HomePage.vue"
-import RegisterPage from "@/pages/app/register/RegisterPage.vue"
-import LoginPage from "@/pages/app/login/LoginPage.vue"
 import NotFoundPage from "@/pages/app/notFound/NotFoundPage.vue"
 import DocumentSignature from "@/pages/app/documentSignature/DocumentSignature.vue"
-import LoginPagev2 from "@/pages/app/login/LoginPagev2.vue"
+import LoginPageV2 from "@/pages/app/login/LoginPagev2.vue"
 import LoginPageV3 from "@/pages/app/login/LoginPageV3.vue"
 import AIConversationPage from "@/pages/app/ai/AIConversationPage.vue"
 
@@ -50,7 +48,7 @@ export default [
         path:AppPaths.Login,
         name:AppPaths.Login.substring(1),
         // component:LoginPage,
-        component:LoginPagev2,
+        component:LoginPageV2,
         meta:{
             layout:AppLayouts.empty
         }
