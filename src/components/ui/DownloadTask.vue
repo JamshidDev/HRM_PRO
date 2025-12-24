485930fa4c5aba57fabfb7ab1838db42340b6955
JAMACODER <64282052+JamshidDev@users.noreply.github.com>
2025-12-24 10:38:02 +0500

    change

--- src/components/ui/DownloadTask.vue
+++ src/components/ui/DownloadTask.vue
@@ -4,6 +4,7 @@ import {ArrowCircleDownUp20Regular} from "@vicons/fluent"
 
 <template>
   <n-popover
+      class="!p-0"
       placement="bottom"
       trigger="click"
   >
@@ -17,8 +18,16 @@ import {ArrowCircleDownUp20Regular} from "@vicons/fluent"
         </template>
       </n-button>
     </template>
-    <div class="w-[260px] h-[200px]" >
+    <div class="w-[260px] h-[200px] cursor-pointer bg-surface-ground" >
+      <div class="w-full border-b border-surface-line px-2 py-2 text-xs font-semibold">
+          Bildirishnomalar
+      </div>
+      <div class="w-full h-[calc(100%-58px)]">
 
+      </div>
+      <div class="w-full text-center border-t border-surface-line px-2 py-1 text-xs">
+        Barchasini ko'rish
+      </div>
     </div>
   </n-popover>
 </template>
