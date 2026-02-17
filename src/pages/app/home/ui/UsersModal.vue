ed099fad004047341faefcbfec417f0fae5f0c68
shakhrillodev <shaxrillonusratov@gmail.com>
2026-02-17 16:38:12 +0500

    CU-86ewmv4m0 - onlayn foydalanuvchilarni mobile yoki webdan kirayotganini ko'rsatish

--- src/pages/app/home/ui/UsersModal.vue
+++ src/pages/app/home/ui/UsersModal.vue
@@ -1,9 +1,8 @@
 <script setup>
   import { useSocketStore } from '@/store/modules/index.js'
-  import { UIDrawer, UIOnlineOffline, UIUser } from '@/components/index.js'
+  import { UIDrawer, UIUser } from '@/components/index.js'
   import ReactionButton from './ReactionButton.vue'
-  import { useAppSetting } from '@/utils/index.js'
-  import { CheckmarkStarburst16Regular } from '@vicons/fluent'
+  import { CheckmarkStarburst16Regular, Phone24Regular, Globe24Regular } from '@vicons/fluent'
 
   const store = useSocketStore()
 
@@ -26,32 +25,44 @@
       <div class="w-full h-[calc(100vh-80px)] overflow-y-auto pt-4 overflow-x-hidden">
         <template v-for="user in store.allOnlineUsers">
           <div class="pl-3 mb-1 py-1 group relative hover:bg-surface-ground">
-            <UIUser
-              :show-online-badge="true"
-              :hide-tooltip="true"
-              :short="false"
-              class="font-medium !text-secondary"
-              :data="{
-                photo: user.photo,
-                lastName: user.short_name,
-                firstName: '',
-                middleName: '',
-                position: user.role
-              }"
-            >
-              <template #position>
-                <div class="leading-[1] text-xs flex gap-1 items-center">
-                  <div
-                    class="bg-primary/5 text-primary/80 px-1 rounded-lg py-[1px] border-primary/20 uppercase font-semibold text-[10px] flex items-center gap-1"
-                  >
-                    <n-icon size="14">
-                      <CheckmarkStarburst16Regular />
-                    </n-icon>
-                    <p class="line-clamp-1">{{ user.organization }}</p>
+            <div class="relative">
+              <UIUser
+                :hide-tooltip="true"
+                :short="false"
+                class="font-medium !text-secondary"
+                :data="{
+                  photo: user.photo,
+                  lastName: user.short_name,
+                  firstName: '',
+                  middleName: '',
+                  position: user.role
+                }"
+              >
+                <template #position>
+                  <div class="leading-[1] text-xs flex gap-1 items-center">
+                    <div
+                      class="bg-primary/5 text-primary/80 px-1 rounded-lg py-[1px] border-primary/20 uppercase font-semibold text-[10px] flex items-center gap-1"
+                    >
+                      <n-icon size="14">
+                        <CheckmarkStarburst16Regular />
+                      </n-icon>
+                      <p class="line-clamp-1">{{ user.organization }}</p>
+                    </div>
                   </div>
-                </div>
-              </template>
-            </UIUser>
+                </template>
+              </UIUser>
+
+              <!-- Device badge -->
+              <div
+                class="absolute bottom-0 left-[28px] w-4 h-4 rounded-full border-2 border-[#6f99ff] flex items-center justify-center"
+                :class="user.type === 'mobile' ? 'bg-success' : 'bg-primary'"
+              >
+                <n-icon size="8" class="text-white">
+                  <Phone24Regular v-if="user.type === 'mobile'" />
+                  <Globe24Regular v-else />
+                </n-icon>
+              </div>
+            </div>
             <div
               class="group-hover:opacity-100 group-hover:right-[-80px] hover:right-[0px] absolute right-[-120px] top-[3px] opacity-0 transition-all duration-300"
             >
