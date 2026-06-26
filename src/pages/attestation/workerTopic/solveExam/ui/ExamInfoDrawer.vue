cd016f98145069be7c730e9e563a3459b58d9674
javohirhasanov <javohirhasanov3303@gmail.com>
2026-06-26 15:59:53 +0500

    feat: enhance exam interface with progress tracking, new components, and improved layout

---
+++ src/pages/attestation/workerTopic/solveExam/ui/ExamInfoDrawer.vue
@@ -0,0 +1,77 @@
+<script setup>
+  import Utils from '@/utils/Utils.js'
+  import { useAppSetting } from '@/utils/index.js'
+
+  defineProps({
+    examDetail: {
+      type: Object,
+      default: null
+    },
+    workerDetail: {
+      type: Object,
+      default: null
+    },
+    questionsCount: {
+      type: Number,
+      default: 0
+    }
+  })
+
+  const visible = defineModel('visible', { type: Boolean, default: false })
+</script>
+
+<template>
+  <n-drawer v-model:show="visible" :width="380" placement="right">
+    <n-drawer-content :title="$t('solveExamPage.info')" closable>
+      <div v-if="workerDetail && examDetail" class="flex flex-col gap-4">
+        <img
+          :src="workerDetail.user.worker.photo || useAppSetting.noAvailableImage"
+          alt="img"
+          class="w-full h-[220px] object-contain rounded-lg border border-surface-line bg-surface-ground"
+        />
+        <n-table size="small">
+          <tr>
+            <th>{{ $t('solveExamPage.exam') }}</th>
+            <td>{{ examDetail.name }}</td>
+          </tr>
+          <tr>
+            <th>{{ $t('solveExamPage.firstName') }}</th>
+            <td>{{ workerDetail.user.worker.first_name }}</td>
+          </tr>
+          <tr>
+            <th>{{ $t('solveExamPage.lastName') }}</th>
+            <td>{{ workerDetail.user.worker.last_name }}</td>
+          </tr>
+          <tr>
+            <th>{{ $t('solveExamPage.middleName') }}</th>
+            <td>{{ workerDetail.user.worker.middle_name }}</td>
+          </tr>
+          <tr>
+            <th>{{ $t('solveExamPage.startedAt') }}</th>
+            <td>{{ Utils.timeWithMonth(workerDetail.created) }}</td>
+          </tr>
+          <tr>
+            <th>{{ $t('solveExamPage.variant') }}</th>
+            <td>{{ examDetail.variant }}</td>
+          </tr>
+          <tr>
+            <th>{{ $t('solveExamPage.totalTime') }}</th>
+            <td>{{ examDetail.minute }}</td>
+          </tr>
+          <tr>
+            <th>{{ $t('solveExamPage.deadline') }}</th>
+            <td>{{ Utils.timeWithMonth(examDetail.deadline) }}</td>
+          </tr>
+          <tr>
+            <th>{{ $t('solveExamPage.attempts') }}</th>
+            <td>{{ examDetail.chances }}</td>
+          </tr>
+          <tr>
+            <th>{{ $t('solveExamPage.questions') }}</th>
+            <td>{{ questionsCount }}</td>
+          </tr>
+        </n-table>
+      </div>
+    </n-drawer-content>
+  </n-drawer>
+</template>
