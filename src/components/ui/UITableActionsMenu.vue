07b5ee1d9674ef0070f4d675506e14f5495bd4bd
ismoildevuz <ismoildevuz@gmail.com>
2026-07-24 17:08:31 +0500

    CU-86eyd9d3h - UI - Table component - custom column select/order management

---
+++ src/components/ui/UITableActionsMenu.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { MoreVertical24Filled } from '@vicons/fluent'
+
+  defineProps({
+    options: { type: Array, required: true }
+  })
+  defineEmits(['select'])
+</script>
+
+<template>
+  <n-dropdown trigger="click" :options="options" @select="(key) => $emit('select', key)">
+    <n-button tertiary circle size="small" @click.stop>
+      <template #icon>
+        <n-icon><MoreVertical24Filled /></n-icon>
+      </template>
+    </n-button>
+  </n-dropdown>
+</template>
